e094932192d2be797ee4f50e6bf9d356f0f3f428
Marcelo Junior <marcelohpjunior@MacBook-Pro-de-Marcelo.local>
2021-12-26 22:26:56 -0300

    Create class MarvelColors

--- lib/src/shared/colors.dart
+++ lib/src/shared/colors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-const red = Color.fromRGBO(242, 38, 75, 1);
-const dark = Color.fromRGBO(49, 49, 64, 1);
-const grey = Color.fromRGBO(183, 183, 200, 1);
-const silver = Color.fromRGBO(248, 248, 248, 1);
+class MarvelColors {
+  static const red = Color.fromRGBO(242, 38, 75, 1);
+  static const dark = Color.fromRGBO(49, 49, 64, 1);
+  static const grey = Color.fromRGBO(183, 183, 200, 1);
+  static const silver = Color.fromRGBO(248, 248, 248, 1);
+}
